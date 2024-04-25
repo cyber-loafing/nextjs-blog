176c517df4c139d0571204a7c19d463813fdf2d0
cyberloafing <934058879@qq.com>
2024-04-26 01:17:53 +0800

    添加shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,20 @@ module.exports = {
         primary: colors.pink,
         gray: colors.gray,
       },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
       typography: ({ theme }) => ({
         DEFAULT: {
           css: {
