cabd3fe088d4693567edffb9723ca8debe896a65
cyberloafing <934058879@qq.com>
2024-04-28 14:21:51 +0800

    添加publication

--- app/_components/Publications.tsx
+++ app/_components/Publications.tsx
@@ -1,12 +1,91 @@
+import { formatDate } from 'pliny/utils/formatDate'
+import Image from 'next/image'
+import PublicationList from '@/data/publicationList'
+
+
 export default function Publications() {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
             Publications
           </h1>
         </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!PublicationList.length && 'No publications found.'}
+          {PublicationList.map((pub) => {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div>
+                <li key={pub.title} className="py-12">
+                  <article>
+                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={pub.date}>{formatDate(pub.date)}</time>
+                        </dd>
+                        <Image
+                          src={pub.img}
+                          alt={pub.title}
+                          width={200}
+                          height={200}
+                        />
+                      </dl>
+                      <div className="space-y-5 xl:col-span-3">
+                        <div className="space-y-6">
+                          <div>
+                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                              {/* 点击时有下划线动画 */}
+                              <a
+                                href={pub.url}
+                                className="text-gray-900 dark:text-gray-100 hover:underline"
+                                target='_blank'
+                              >
+                                {pub.title}
+                              </a>
+                            </h2>
+                            <div className='text-xl text-primary-400'>
+                              {pub.publish_in}
+                            </div>
+                            <div className="flex flex-wrap">
+                              {pub.authors.map((author,idx) => (
+                                // 如果是pub.bold_idx，就加粗
+                                // 如果不是最后一个，就加逗号
+                                <span key={author} className={author === "Hanze Jia" ? 'font-bold' : ''}>
+                                  {author}
+                                  {idx !== pub.authors.length - 1 && ', '}
+                                </span>
+                              ))}
+                            </div>
+                            {/* pdf 在新标签页面显示*/}
+                            <div>
+                              <a
+                                href={pub.url}
+                                className='text-primary-400 underline hover:text-primary-600 mr-3'
+                                target='_blank'
+                              >
+                                Detail
+                              </a>
+                              <a
+                                href={pub.pdf}
+                                className='text-primary-400 underline hover:text-primary-600 mr-3'
+                                target='_blank'
+                              >
+                                PDF
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+                </li>
+              </div>
+            )
+          })}
+        </ul>
       </div>
     </>
   )
---
+++ data/publicationList.ts
@@ -0,0 +1,23 @@
+const PublicationList = [
+    {
+      "title": "VolleyNaut: Pioneering Immersive Training for Inclusive Sitting Volleyball Skill Development",
+      "authors": ["Ut Gong", "Hanze Jia", "Yujie Wang","Tan Tang","Xiao Xie","Yingcai Wu"],
+      "bold_idx": 1,
+      "url": "https://ieeexplore.ieee.org/document/10494154",
+      "pdf": "https://ieeexplore.ieee.org/document/10494154",
+      "date": "2024-03-16",
+      "publish_in": "2024 IEEE Conference Virtual Reality and 3D User Interfaces (VR)",
+      "img" : "/static/pub/volleynaut.png"
+    },
+    {
+      "title": "VolleyNaut: Pioneering Immersive Training for Inclusive Sitting Volleyball Skill Development",
+      "authors": ["Ut Gong", "Hanze Jia", "Yujie Wang","Tan Tang","Xiao Xie","Yingcai Wu"],
+      "bold_idx": 1,
+      "url": "https://www.google.com",
+      "pdf": "https://www.google.com",
+      "date": "2024-03-16",
+      "publish_in": "2024 IEEE Conference Virtual Reality and 3D User Interfaces (VR)",
+      "img" : "/static/pub/volleynaut.png"
+    },
+  ]
+export default PublicationList;
