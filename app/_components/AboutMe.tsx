347d4e487e4f2a6870a6423d3d595c2014b68119
cyberloafing <hzjia1118@gmail.com>
2024-06-28 22:35:38 +0800

    main page change

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -13,19 +13,19 @@ export default function AboutMe() {
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 py-8">
             <Image
-              src="/static/images/xiaogong.png"
+              src="/static/images/jhz.png"
               alt="avatar"
               width={192}
               height={192}
               className="h-48 w-48 rounded-full"
             />
-            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">Jia Hanze</h3>
+            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">Hanze Jia 贾涵泽</h3>
             <div className="text-gray-500 dark:text-gray-400">Zhejiang University</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:$123`} />
-              <SocialIcon kind="github" href="{github}" />
-              <SocialIcon kind="linkedin" href="{linkedin}" />
-              <SocialIcon kind="twitter" href="{twitter}" />
+            <div className="flex flex-col space-y-3 pt-5">
+              <SocialIcon kind="mail" href={`mailto:hzjia1118@gmail.com`} disp='hzjia1118@gmail.com' />
+              <SocialIcon kind="github" href="https://github.com/cyber-loafing" disp='Github' />
+              <SocialIcon kind="twitter" href="{twitter}" disp='Twitter' />
+              <SocialIcon kind="googlescholar" href="{linkedin}" disp='Google Scholar' />
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -8,6 +8,7 @@ import {
   Mastodon,
   Threads,
   Instagram,
+  GoogleScholar
 } from './icons'
 
 const components = {
@@ -20,15 +21,17 @@ const components = {
   mastodon: Mastodon,
   threads: Threads,
   instagram: Instagram,
+  googlescholar: GoogleScholar
 }
 
 type SocialIconProps = {
   kind: keyof typeof components
   href: string | undefined
   size?: number
+  disp?: string
 }
 
-const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
+const SocialIcon = ({ kind, href, size = 6, disp = "" }: SocialIconProps) => {
   if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
     return null
 
@@ -42,9 +45,14 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
       href={href}
     >
       <span className="sr-only">{kind}</span>
-      <SocialSvg
-        className={`fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-${size} w-${size}`}
-      />
+      {/* 图标与描述在一行显示 */}
+      <div className="flex items-center space-x-4">
+        <SocialSvg
+          className={`fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-${size} w-${size}`}
+        />
+        <span>{disp}</span>
+      </div>
+
     </a>
   )
 }
